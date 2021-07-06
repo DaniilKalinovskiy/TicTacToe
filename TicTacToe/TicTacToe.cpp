ca3531e62c31af21ae09835c056e3d2b3b6f1013
Даниил Калиновский <daniyil.kalinovskyy@vokigames.com>
2021-07-06 19:11:54 +0300

    Создал структуру, реализовал рисование поля, начал реализовывать ввод по координатам

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,12 +1,94 @@
+//◦ Playrix ◦
 // TicTacToe.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
 
+using namespace std;
+
+void Setup();
+void Draw();
+void Input();
+void Score();
+
+int PlayerScore = 0;
+char PlayingField[3][3];
+bool PlayerNo = true;
+bool isGameEnded = false;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    while (!isGameEnded)
+    {
+        Draw();
+        Input();
+    }
+    return 0;
 }
+
+void Setup()
+{
+    
+}
+
+void Draw()
+{
+    for (int coordX = 0; coordX < 3; coordX++)
+    {
+        for (int coordY = 0; coordY < 3; coordY++)
+        {
+            switch (PlayingField[coordX][coordY])
+            {
+            case -1:
+                cout << "[O]";
+                break;
+            case 0:
+                cout << "[ ]";
+                break;
+            case 1:
+                cout << "[X]";
+                break;
+            }
+        }
+        cout << endl;
+    }                           //Добавить вывод счета
+}
+
+void Input()
+{
+    int playerNo;
+    int x;
+    int y;
+
+    if (PlayerNo) playerNo = 1;
+    else playerNo = 2;
+
+    cout << "Player " << playerNo << " Please, enter the X coordinate:" << endl;
+    cin >> x;
+    cout << "Please, enter the Y coordinate:" << endl;
+    cin >> y;
+
+    //Сделать возможность заново вводить координаты, если ячейка заполнена
+
+    if (PlayingField[x][y] == 0)                //Проверяем, не занята ли ячейка
+    {
+        if (PlayerNo) PlayingField[x][y] = 1;
+        else PlayingField[x][y] = -1;
+    }
+    else cout << "This field is not empty!";
+
+
+
+    PlayerNo = !PlayerNo;
+}
+
+void Score()
+{
+}
+
+
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
